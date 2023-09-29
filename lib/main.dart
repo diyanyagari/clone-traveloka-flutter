009a3091255cd86d519e1a4981198a7640408941
yulianti <yuliantisimatupang@gmail.com>
2023-09-29 22:52:28 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:latihanflutter/utils/colors.dart';
+import 'package:latihanflutter/widgets/searchbar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Latihan Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Museo'),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        elevation: 0,
+        toolbarHeight: 71,
+        title: Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  color: Colors.white,
+                ),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: DecoratedBox(
+                // color
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromRGBO(74,157,220, 1);
+const grayColor = Color.fromRGBO(162,162,162, 1);
